9d7fa302f2e7ab978dfa39e6c14e93de73535694
karolg02 <karolglanowski02@gmail.com>
2024-12-17 18:31:37 +0100

    + /api/teacher done

---
+++ zadanie5/src/main/java/com/karol/zadanie5/Zadanie5Application.java
@@ -0,0 +1,13 @@
+package com.karol.zadanie5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Zadanie5Application.class, args);
+	}
+
+}
---
+++ zadanie5/src/test/java/com/karol/zadanie5/Zadanie5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karol.zadanie5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
